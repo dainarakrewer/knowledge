32e6fc503551c69121b72c6569e807089eeefb8c
Dainara Krewer <55606090+dainarakrewer@users.noreply.github.com>
2023-04-18 10:26:58 -0300

    18-04

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt-nodejs')
 
 module.exports = app => {
-    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+    const {existsOrError, notExistsOrError, equalsOrError} = app.api.validation
 
     const encryptPassword = password => {
         const salt = bcrypt.genSaltSync(10)
@@ -9,11 +9,11 @@ module.exports = app => {
     }
 
     const save = async (req, res) => {
-        const user = { ...req.body }
-        if(req.params.id) user.id = req.params.id
+        const user = {...req.body}
+        if (req.params.id) user.id = req.params.id
 
-        if(!req.originalUrl.startsWith('/users')) user.admin = false
-        if(!req.user || !req.user.admin) user.admin = false
+        if (!req.originalUrl.startsWith('/users')) user.admin = false
+        if (!req.user || !req.user.admin) user.admin = false
 
         try {
             existsOrError(user.name, 'Nome não informado')
@@ -23,30 +23,35 @@ module.exports = app => {
             equalsOrError(user.password, user.confirmPassword, 'Senhas não conferem')
 
             const userFromDB = await app.db('users')
-                .where({ email: user.email }).first()
-            if(!user.id){
+                .where({email: user.email}).first()
+            if (!user.id) {
                 notExistsOrError(userFromDB, 'Usuário já cadastrado')
             }
         } catch (msg) {
             return res.status(400).send(msg)
         }
 
-        user.password = encryptPassword(user.password)
-        delete user.confirmPassword
+        try {
+            user.password = encryptPassword(user.password)
+            delete user.confirmPassword
 
-        if(user.id) {
-            app.db('users')
-                .update(user)
-                .where({ id: user.id })
-                .whereNull('deletedAt')
-                .then(_ => res.status(204).send())
-                .catch(err => res.status(500).send(err))
-        } else {
-            app.db('users')
-                .insert(user)
-                .then(_ => res.status(204).send)
-                .catch(err => res.status(500).send(err))
+            if (user.id) {
+                app.db('users')
+                    .update(user)
+                    .where({id: user.id})
+                    .whereNull('deletedAt')
+                    .then(_ => res.status(204).send())
+                    .catch(err => res.status(500).send(err))
+            } else {
+                app.db('users')
+                    .insert(user)
+                    .then(_ => res.status(204).send())
+                    .catch(err => res.status(500).send(err))
+            }
+        } catch (e) {
+            res.send(e)
         }
+
     }
 
     const get = (req, res) => {
@@ -60,7 +65,7 @@ module.exports = app => {
     const getById = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
-            .where({ id: req.params.id })
+            .where({id: req.params.id})
             .whereNull('deletedAt')
             .first()
             .then(user => res.json(user))
@@ -70,19 +75,19 @@ module.exports = app => {
     const remove = async (req, res) => {
         try {
             const articles = await app.db('articles')
-                .where({ userId: req.params.id })
+                .where({userId: req.params.id})
             notExistsOrError(articles, 'Usuário possui artigos.')
 
             const rowsUpdated = await app.db('users')
-                .update({ deletedAt: new Date() })
-                .where({ id: req.params.id })
+                .update({deletedAt: new Date()})
+                .where({id: req.params.id})
             existsOrError(rowsUpdated, 'Usuário não foi encontrado')
 
             res.status(204).send()
-        } catch(msg) {
+        } catch (msg) {
             res.status(400).send(msg)
         }
     }
 
-    return { save, get, getById, remove }
+    return {save, get, getById, remove}
 }
--- versao-inicial/frontend/src/components/template/UserDropdown.vue
+++ versao-inicial/frontend/src/components/template/UserDropdown.vue
@@ -8,11 +8,11 @@
           <i class="fa fa-angle-down"></i>
       </div>
       <div class="user-dropdown-content">
-          <router-link to="/admin">
+          <router-link to="/admin" v-if="user.admin">
               <i class="fa fa-cogs"></i>
               Administração
           </router-link>
-          <a href>
+          <a href @click.prevent="logout">
               <i class="fa fa-sign-out"></i>
               Sair
           </a>
@@ -21,13 +21,21 @@
 </template>
 
 <script>
+import { userKey } from "@/global";
 import { mapState } from 'vuex'
 import Gravatar from 'vue-gravatar'
 
 export default {
     name: "UserDropdown",
     components: { Gravatar },
-    computed: mapState(['user'])
+    computed: mapState(['user']),
+    methods: {
+        logout() {
+            localStorage.removeItem(userKey)
+            this.$store.commit('setUser', null)
+            this.$router.push({ name: 'auth'})
+        }
+    }
 }
 </script>
 
